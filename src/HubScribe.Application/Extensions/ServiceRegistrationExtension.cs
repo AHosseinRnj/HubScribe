39d4bbb7c1a13eed9cbce6f1a22aa47774ae8d2d
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2024-09-27 00:35:13 +0330

    Merge development into main (#4)
    - Add Api, Domain, Infra and application layer (#3)
    - Add api versioning
    - Add and register MediatR

---
+++ src/HubScribe.Application/Extensions/ServiceRegistrationExtension.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HubScribe.Application.Extensions;
+
+public static class ServiceRegistrationExtension
+{
+    public static void ConfigureApplicationLayer(this IServiceCollection services)
+    {
+        #region [ MediatR ]
+        var executingAssembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(configure =>
+        {
+            configure.RegisterServicesFromAssembly(executingAssembly); 
+        });
+
+        services.AddValidatorsFromAssembly(executingAssembly);
+        #endregion
+    }
+}
