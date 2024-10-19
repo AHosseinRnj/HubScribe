73fdec9774eb7751a96750311f5170a793b32075
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2024-10-19 22:24:46 +0330

    Development into main (#9)
    - Replace tags with tag ids in article model
    - Add likes in article model
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/HubScribe.Domain/Articles/Entities/Article.cs
+++ src/HubScribe.Domain/Articles/Entities/Article.cs
@@ -9,12 +9,13 @@ public class Article
     public string AuthorId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    public int LikesCount { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
     public DateTime? PublishedDate { get; set; }
     public ArticleStatus Status { get; set; }
+    public bool IsDeleted { get; set; }
 
     public List<Comment> Comments { get; set; } = [];
-    public List<Tag> Tags { get; set; } = [];
+    public List<Like> Likes { get; set; } = [];
+    public List<Guid> TagIds { get; set; } = [];
 }
